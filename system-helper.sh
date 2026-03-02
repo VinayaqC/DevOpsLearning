a348dba82c01b0b78f78b38152a215627301470a
Vinayaq Chakraborty <vinayaqc@gmail.com>
2026-03-02 15:50:15 +0000

    Add uptime command to system-helper script

---
+++ system-helper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+echo "Usage: $0 {disk|memory|user}"
+exit 1
+fi
+
+case "$1" in
+
+disk)
+usage=$(df -h / | tail -1 | awk '{print $5}')
+echo "Disk usage is: $usage"
+;;
+
+memory)
+echo "Memory usage"
+free -h
+;;
+
+user)
+echo "Current user"
+whoami
+;;
+
+uptime)
+echo "System uptime"
+uptime
+;;
+
+*)
+echo "Invalid option"
+echo "Usage: $0 {disk|memory|user}"
+exit 2
+;;
+
+esac
+
+exit 0
